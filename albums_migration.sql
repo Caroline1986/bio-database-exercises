115c610df9517c5ac1d532465ab66a295994d94b
Caroline E <caroline.esquivel01@gmail.com>
2020-10-27 11:21:01 -0500

    added album insert info

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR (50),
-    name VARCHAR (100),
-    release_date INT,
-    sales FLOAT (5,2),
+    artist VARCHAR (50) NOT NULL,
+    name VARCHAR (100) NOT NULL DEFAULT 'Untitled',
+    release_date INT UNSIGNED,
+    sales FLOAT UNSIGNED,
     genre VARCHAR (20),
     PRIMARY KEY (id)
     );
