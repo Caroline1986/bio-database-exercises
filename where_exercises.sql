512246dd3e4372c09145f2726b072d00e449e252
Caroline E <caroline.esquivel01@gmail.com>
2020-10-28 11:13:29 -0500

    starting part 2

---
+++ where_exercises.sql
@@ -0,0 +1,30 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya','Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN'1990-01-01' AND '1999-12-31';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+# exercise part 2
+SELECT *
+FROM employees
+WHERE first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya';
+
+SELECT * FROM employees
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+  AND gender = 'M';
