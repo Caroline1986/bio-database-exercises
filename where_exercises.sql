787a894211d46429e6e29dbe94157dfcbcb4a168
Caroline E <caroline.esquivel01@gmail.com>
2020-10-28 11:26:23 -0500

    competed where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -28,3 +28,22 @@ WHERE (first_name = 'Irena'
     OR first_name = 'Vidya'
     OR first_name = 'Maya')
   AND gender = 'M';
+
+#  starts OR ends with 'E'
+SELECT *
+FROM employees
+WHERE (last_name LIKE 'E%') OR (last_name LIKE '%E');
+
+#  starts AND ends with 'E'
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+SELECT *
+FROM employees
+WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+AND birth_date LIKE '%-12-25';
+
+SELECT *
+FROM (SELECT * FROM employees WHERE last_name LIKE '%q%') AS t1
+WHERE last_name NOT LIKE '%qu%';
