50da51e86331c6e58e0f904b9a2994a816416e57
Caroline E <caroline.esquivel01@gmail.com>
2020-10-28 11:35:03 -0500

    ordering by clause

---
+++ order_by_exercises.sql
@@ -0,0 +1,50 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya','Maya')
+ORDER BY first_name,last_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN'1990-01-01' AND '1999-12-31';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+# exercise part 2
+SELECT *
+FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
+
+SELECT * FROM employees
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+  AND gender = 'M';
+
+#  starts OR ends with 'E'
+SELECT *
+FROM employees
+WHERE (last_name LIKE 'E%') OR (last_name LIKE '%E');
+
+#  starts AND ends with 'E'
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+SELECT *
+FROM employees
+WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+  AND birth_date LIKE '%-12-25';
+
+SELECT *
+FROM (SELECT * FROM employees WHERE last_name LIKE '%q%') AS t1
+WHERE last_name NOT LIKE '%qu%';
