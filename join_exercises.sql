2529343bcc91209cc64584f40effc9b10dadcdff
Caroline E <caroline.esquivel01@gmail.com>
2020-10-30 11:05:49 -0500

    subquery exercises with walkthrough

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,3 +1,58 @@
+CREATE TABLE roles
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users
+(
+    id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(100) NOT NULL,
+    email   VARCHAR(100) NOT NULL,
+    role_id INT UNSIGNED DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+INSERT INTO roles (name)
+VALUES ('admin');
+INSERT INTO roles (name)
+VALUES ('author');
+INSERT INTO roles (name)
+VALUES ('reviewer');
+INSERT INTO roles (name)
+VALUES ('commenter');
+
+INSERT INTO users (name, email, role_id)
+VALUES ('bob', 'bob@example.com', 1),
+       ('joe', 'joe@example.com', 2),
+       ('sally', 'sally@example.com', 3),
+       ('adam', 'adam@example.com', 3),
+       ('jane', 'jane@example.com', null),
+       ('mike', 'mike@example.com', null);
+
+INSERT INTO users (name, email, role_id)
+VALUES ('bob', 'bob@example.com', 2),
+       ('joe', 'joe@example.com', 2),
+       ('sally', 'sally@example.com', 2),
+       ('jane', 'jane@example.com', null);
+
+SELECT *
+FROM users;
+
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+         JOIN roles ON users.role_id = roles.id;
+
+SELECT u.name as name, u.email, r.name AS 'role'
+FROM users AS u
+         LEFT JOIN roles AS r ON u.role_id = r.id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         RIGHT JOIN roles ON users.role_id = roles.id;
+
 USE employees;
 
 # each department along with the name of the current manager for that department.
@@ -12,10 +67,22 @@ WHERE de.to_date = '9999-01-01'
   AND gender = 'f'
 ORDER BY d.dept_name;
 
+#4
+# SELECT * FROM departments WHERE dept_name = 'Customer Servive'; -- d009
+
+SELECT t.title AS Title, COUNT(t.emp_no) AS Count
+FROM titles AS t
+         JOIN dept_emp AS de on t.emp_no = de.emp_no
+         JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE de.dept_no = 'd009'
+  AND t.to_date = '9999-01-01'
+  AND de.to_date = '9999-01-01'
+GROUP BY t.title;
+
 # Find the current salary of all current managers.
-SELECT d.dept_name AS 'Department Name',
+SELECT d.dept_name                            AS 'Department Name',
        CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager',
-       s.salary AS salary
+       s.salary                               AS salary
 FROM employees as e
          JOIN dept_manager as de
               ON de.emp_no = e.emp_no
---
+++ subqueries_exercises.sql
@@ -0,0 +1,33 @@
+# Find all the employees with the same hire date as employee 101010 using a subquery. 69 Rows
+USE employees;
+
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name
+FROM employees
+WHERE hire_date = (SELECT hire_date
+                   FROM employees
+                   WHERE emp_no =101010);
+
+# Find all the titles held by all employees with the first name Aamod.
+# 314 total titles, 6 unique titles
+
+# SELECT title, COUNT(title) #--- find all Aamods
+SELECT DISTINCT title #--used to find unique titles
+FROM titles
+WHERE emp_no IN (SELECT emp_no
+                 FROM employees
+                 WHERE first_name = 'Aamod');
+# GROUP BY title; #--used with finding all Aamods
+
+#use as subquery
+# SELECT emp_no, first_name
+# FROM employees
+# WHERE first_name;
+
+# Find all the current department managers that are female
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, gender
+FROM employees
+WHERE emp_no IN
+      (SELECT emp_no
+       FROM dept_manager
+       WHERE to_date = '9999-01-01')
+  AND gender = 'F';
